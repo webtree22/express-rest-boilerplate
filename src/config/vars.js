4041e25a5ccc9b93372b37f870799172b5e61732
Sudeep Gurung <webtree22@gmail.com>
2022-01-30 12:21:20 +0545

    wip

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,9 +1,8 @@
-const path = require('path');
+const path = require("path");
 
 // import .env variables
-require('dotenv-safe').config({
-  path: path.join(__dirname, '../../.env'),
-  example: path.join(__dirname, '../../.env.example'),
+require("dotenv-safe").config({
+  path: path.join(__dirname, "../../.env"),
 });
 
 module.exports = {
@@ -12,9 +11,12 @@ module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
-    uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
+    uri:
+      process.env.NODE_ENV === "test"
+        ? process.env.MONGO_URI_TESTS
+        : process.env.MONGO_URI,
   },
-  logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  logs: process.env.NODE_ENV === "production" ? "combined" : "dev",
   emailConfig: {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
